4d24e868a50c3dffec3b8246761ae7fb94ae1c69
Joshua Robbins <joshrobbins11@gmail.com>
2026-02-13 15:11:51 -0500

    copying in internal python updates

--- python/zonoopt/__init__.py
+++ python/zonoopt/__init__.py
@@ -4,5 +4,6 @@ from ._core import *
 from ._core import __doc__
 
 from .zono_plot import plot, get_vertices
+from .zono_json import to_json, from_json
 
-__all__ =  [name for name, obj in inspect.getmembers(_core)] + ['plot', 'get_vertices']
+__all__ =  [name for name, obj in inspect.getmembers(_core)] + ['plot', 'get_vertices', 'to_json', 'from_json']
