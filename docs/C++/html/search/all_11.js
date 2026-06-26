ab53451b7987e85c6a765bd400c53651233eeb6f
Joshua Robbins <joshrobbins11@gmail.com>
2026-06-26 18:38:47 -0400

    2.4.0 -> 2.4.1

--- docs/C++/html/search/all_11.js
+++ docs/C++/html/search/all_11.js
@@ -5,11 +5,12 @@ var searchData=
   ['real_5fparams_2',['real_params',['../structZonoOpt_1_1SCIPSettings.html#a5b90b7d1fe3d88612a8307e18ba19c15',1,'ZonoOpt::SCIPSettings']]],
   ['reduce_5forder_3',['reduce_order',['../classZonoOpt_1_1Zono.html#a98480f31374733165d0b0b4c22d82bc9',1,'ZonoOpt::Zono']]],
   ['references_4',['References',['../index.html#autotoc_md7',1,'']]],
-  ['remove_5fcols_5',['remove_cols',['../classZonoOpt_1_1HybZono.html#abc0ef4a673aaf8cc0424b598de9900f2',1,'ZonoOpt::HybZono']]],
-  ['remove_5fredundancy_6',['remove_redundancy',['../classZonoOpt_1_1HybZono.html#af1a5102a6ee696b6984bdeeab5a3eb85',1,'ZonoOpt::HybZono::remove_redundancy()'],['../classZonoOpt_1_1Point.html#a4aeeab49744e090529df98044a562e48',1,'ZonoOpt::Point::remove_redundancy()']]],
-  ['rho_7',['rho',['../structZonoOpt_1_1OptSettings.html#aa9daad3982cfea491e0aa34451dc53dd',1,'ZonoOpt::OptSettings']]],
-  ['rng_5fseed_8',['rng_seed',['../structZonoOpt_1_1OptSettings.html#a461a2b8cf4bcfa6010ab06b4a00209e3',1,'ZonoOpt::OptSettings']]],
-  ['rows_9',['rows',['../classZonoOpt_1_1IntervalMatrix.html#a0bc12d80aa6560a114c9a0560aaf99d8',1,'ZonoOpt::IntervalMatrix']]],
-  ['run_5ftime_10',['run_time',['../structZonoOpt_1_1OptSolution.html#a4c924318807a20bd19229fe1d7995de2',1,'ZonoOpt::OptSolution']]],
-  ['runtime_11',['runtime',['../GurobiSolver_8cpp.html#a01143f6725f562bed4255476fa737925',1,'runtime:&#160;GurobiSolver.cpp'],['../SCIPSolver_8cpp.html#a01143f6725f562bed4255476fa737925',1,'runtime:&#160;SCIPSolver.cpp']]]
+  ['related_20projects_5',['Related Projects',['../index.html#autotoc_md11',1,'']]],
+  ['remove_5fcols_6',['remove_cols',['../classZonoOpt_1_1HybZono.html#abc0ef4a673aaf8cc0424b598de9900f2',1,'ZonoOpt::HybZono']]],
+  ['remove_5fredundancy_7',['remove_redundancy',['../classZonoOpt_1_1Point.html#a4aeeab49744e090529df98044a562e48',1,'ZonoOpt::Point::remove_redundancy()'],['../classZonoOpt_1_1HybZono.html#af1a5102a6ee696b6984bdeeab5a3eb85',1,'ZonoOpt::HybZono::remove_redundancy()']]],
+  ['rho_8',['rho',['../structZonoOpt_1_1OptSettings.html#aa9daad3982cfea491e0aa34451dc53dd',1,'ZonoOpt::OptSettings']]],
+  ['rng_5fseed_9',['rng_seed',['../structZonoOpt_1_1OptSettings.html#a461a2b8cf4bcfa6010ab06b4a00209e3',1,'ZonoOpt::OptSettings']]],
+  ['rows_10',['rows',['../classZonoOpt_1_1IntervalMatrix.html#a0bc12d80aa6560a114c9a0560aaf99d8',1,'ZonoOpt::IntervalMatrix']]],
+  ['run_5ftime_11',['run_time',['../structZonoOpt_1_1OptSolution.html#a4c924318807a20bd19229fe1d7995de2',1,'ZonoOpt::OptSolution']]],
+  ['runtime_12',['runtime',['../GurobiSolver_8cpp.html#a01143f6725f562bed4255476fa737925',1,'runtime:&#160;GurobiSolver.cpp'],['../SCIPSolver_8cpp.html#a01143f6725f562bed4255476fa737925',1,'runtime:&#160;SCIPSolver.cpp']]]
 ];
--- docs/C++/html/search/all_14.js
+++ docs/C++/html/search/all_14.js
@@ -4,5 +4,6 @@ var searchData=
   ['ub_5fd_1',['ub_d',['../GurobiSolver_8cpp.html#ab1d8a40b0fb4c243acf66b4379157274',1,'ub_d:&#160;GurobiSolver.cpp'],['../SCIPSolver_8cpp.html#ab1d8a40b0fb4c243acf66b4379157274',1,'ub_d:&#160;SCIPSolver.cpp']]],
   ['union_5fof_5fmany_2',['union_of_many',['../classZonoOpt_1_1HybZono.html#aa9d2fc96ff1937d8a8f0b6af96da6e8d',1,'ZonoOpt::HybZono::union_of_many'],['../group__ZonoOpt__SetOperations.html#ga74e540bb5cb3dee56069affcea646a25',1,'ZonoOpt::union_of_many()']]],
   ['upper_3',['upper',['../classZonoOpt_1_1Box.html#a7e2fa323afdb2ca4b2f9d09d44745442',1,'ZonoOpt::Box::upper()'],['../classZonoOpt_1_1Interval.html#a59ad3a9166813c6146c47a2207cfddd0',1,'ZonoOpt::Interval::upper()']]],
-  ['use_5finterval_5fcontractor_4',['use_interval_contractor',['../structZonoOpt_1_1OptSettings.html#a2d6b27de8e3ff88c892cfd714ee39bd5',1,'ZonoOpt::OptSettings']]]
+  ['use_5finterval_5fcontractor_4',['use_interval_contractor',['../structZonoOpt_1_1OptSettings.html#a2d6b27de8e3ff88c892cfd714ee39bd5',1,'ZonoOpt::OptSettings']]],
+  ['using_20zonoopt_5',['Projects Using ZonoOpt',['../index.html#autotoc_md10',1,'']]]
 ];
